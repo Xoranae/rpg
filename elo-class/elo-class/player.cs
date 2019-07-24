26b87c136c06d7ea04ff131332642dfd3f4bd84e
Rox1302 <elodie.marquez94@gmail.com>
2019-07-24 15:22:17 +0200

    départ

---
+++ elo-class/elo-class/player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elo_class
+{
+    public class player
+    {
+        public string _name { get; set; }
+        public int _level { get; set; }
+        public int _life { get; set; }
+
+        /// <summary>
+        /// create a Player object with given level
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="level"></param>
+        public player(string name, int level, int life)
+        {
+            verifyParameter(name);
+            this._name = name;
+            this._level = level;
+            this._life = life;
+        }
+
+        /// <summary>
+        /// create a Player object with default level 1
+        /// </summary>
+        /// <param name="name"></param>
+        public player(string name)
+        {
+            verifyParameter(name);
+            this._name = name;
+            _level = 1;
+            _life = 10;
+        }
+
+        /// <summary>
+        /// verify if a parameter of type string
+        /// </summary>
+        /// <param name="name"></param>
+        void verifyParameter(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("name can not be null", name);
+            }
+        }
+    }
+}
